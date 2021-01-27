ca15e724872ae6c7c54b6ef83644e62672af49b3
AT <734237292@qq.com>
2021-01-27 13:47:11 +0800

    gitignore out

---
+++ src/Enum/Enum.ts
@@ -0,0 +1,49 @@
+// TODO: 应该可以获取配置信息，后期可以改为从配置信息中获取变量名
+export const SETTINGS_LIST = [
+  {
+    name: 'Prefix Logo',
+    default: '',
+    description: '前缀标识'
+  },
+  {
+    name: 'Color',
+    default: '',
+    description: '字体颜色'
+  },
+  {
+    name: 'Color Bg',
+    default: '',
+    description: '背景颜色'
+  },
+  {
+    name: 'Font Size',
+    default: '',
+    description: '字号大小'
+  },
+  {
+    name: 'Show Semi',
+    default: false,
+    description: '末尾是否加分号'
+  },
+  {
+    name: 'Number Argument',
+    default: 'console.log 接收 2 个参数',
+    description: 'console.log 接收的变量数量'
+  }
+]
+// 变量占位符
+export const PLACEHOLDER = '#'
+
+// 用户选择 console.log 的参数个数
+export const NUMBER_ARGUMENT = {
+  oneArgument: 'console.log 接收 1 个参数',
+  twoArgument: 'console.log 接收 2 个参数',
+  threeArgument: 'console.log 接收 3 个参数'
+}
+
+// 样式 key
+export const STYLES = {
+  color: 'color',
+  colorBg: 'background',
+  fontSize: 'font-size'
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,55 +1,10 @@
 import { window, Range, commands, workspace } from 'vscode'
-
-// TODO: 应该可以获取配置信息，后期可以改为从配置信息中获取变量名
-const SETTINGS_LIST = [
-  {
-    name: 'Prefix Logo',
-    default: '',
-    description: '前缀标识'
-  },
-  {
-    name: 'Color',
-    default: '',
-    description: '字体颜色'
-  },
-  {
-    name: 'Color Bg',
-    default: '',
-    description: '背景颜色'
-  },
-  {
-    name: 'Font Size',
-    default: '',
-    description: '字号大小'
-  },
-  {
-    name: 'Show Semi',
-    default: false,
-    description: '末尾是否加分号'
-  },
-  {
-    name: 'Number Argument',
-    default: 'console.log 接收 2 个参数',
-    description: 'console.log 接收的变量数量'
-  }
-]
-
-// 变量占位符
-const PLACEHOLDER = '#'
-
-// 用户选择 console.log 的参数个数
-const NUMBER_ARGUMENT = {
-  oneArgument: 'console.log 接收 1 个参数',
-  twoArgument: 'console.log 接收 2 个参数',
-  threeArgument: 'console.log 接收 3 个参数'
-}
-
-// 样式 key
-const STYLES = {
-  color: 'color',
-  colorBg: 'background',
-  fontSize: 'font-size'
-}
+import {
+  SETTINGS_LIST,
+  PLACEHOLDER,
+  NUMBER_ARGUMENT,
+  STYLES
+} from './Enum/Enum'
 
 // 执行 log 写入
 const insertText = val => {
