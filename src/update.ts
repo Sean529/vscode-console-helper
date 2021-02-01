ffdf09707fb1105f336af5cfc78abce64b590738
AT <734237292@qq.com>
2021-02-01 13:59:02 +0800

    feat 插件升级提醒

---
+++ src/update.ts
@@ -0,0 +1,33 @@
+import { commands, extensions, window } from 'vscode'
+import axios from 'axios'
+const compareVersions = require('compare-versions')
+
+export default () => {
+  const consoleHelperExt = extensions.getExtension('at-9420.console-helper')
+  const version = consoleHelperExt?.packageJSON?.version
+  // tags 没找到分页查询，数据大的时候考虑删除过旧的tags
+  axios
+    .get(
+      'https://api.github.com/repos/shangZhengXie/vscode-console-helper/tags'
+    )
+    .then(res => {
+      const [newTag] = res.data
+      const latestVersion = newTag.name.slice(1) // git release 版本号
+      if (compareVersions(version, latestVersion) === -1) {
+        // 若插件的版本号小于 git release 版本则触发插件升级提醒
+        window
+          .showInformationMessage(
+            '检查到 [ console helper ] 插件有新版本，是否立即升级？',
+            '去升级',
+            '取消'
+          )
+          .then(res => {
+            if (res === '去升级') {
+              commands.executeCommand(
+                'workbench.extensions.action.showInstalledExtensions'
+              )
+            }
+          })
+      }
+    })
+}
