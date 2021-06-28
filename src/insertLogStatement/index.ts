4f66a750dcb5bd946f2979497ef73b56324f58d7
AT <shangzheng.xie@timschina.com>
2021-06-29 07:00:54 +0800

    feat 要打印的变量不做处理

--- src/insertLogStatement/index.ts
+++ src/insertLogStatement/index.ts
@@ -22,7 +22,7 @@ export const insertLogStatement = context => {
       if (text) {
         // 将选择的字符美化后打印
         commands.executeCommand('editor.action.insertLineAfter').then(() => {
-          const selectVariable = text.replace(/'|"/g, '') // 编辑器中选中的文本（要打印的变量）
+          const selectVariable = text // 编辑器中选中的文本（要打印的变量）
           // 获取配置信息
           const fontSize = getSettingValue('Font Size')
           const fontSizeStr = setFontSizeStr(fontSize) // fontSize 的值
