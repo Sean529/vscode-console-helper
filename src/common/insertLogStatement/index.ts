aa74bb5629ff73899a908f0db49a26715c501f25
lvvvkvv <lvvvkvv@lvvvkvvdeMacBook-Pro.local>
2024-07-16 16:54:52 +0800

    支持多选打印

--- src/common/insertLogStatement/index.ts
+++ src/common/insertLogStatement/index.ts
@@ -1,39 +1,26 @@
-import { window, commands, TextEditor } from 'vscode'
-import { showErrorMessage, insertText, formatVar } from '../index'
+import { window, commands } from 'vscode'
+import { showErrorMessage } from '../index'
 import { handleCursorInsert } from '../insertVariable'
 
 // log 插入
-const { activeTextEditor, onDidChangeActiveTextEditor } = window
-let currentEditor: TextEditor
 export const insertLogStatement = context => {
-  currentEditor = activeTextEditor
-  // 当触发文本时重新赋值
-  onDidChangeActiveTextEditor(editor => (currentEditor = editor))
   const insert = commands.registerTextEditorCommand(
     'consoleLog.insertLogStatement',
-    () => handle(context)
+    handle
   )
   // 订阅命令
   context.subscriptions.push(insert)
 }
 
-const handle = context => {
-  const editor = window.activeTextEditor
-  if (!editor) {
+const handle = async () => {
+  const textEditor = window.activeTextEditor
+  if (!textEditor) {
     showErrorMessage()
     return
   }
 
-  const { selection } = editor
-  const selectVariable = editor.document.getText(selection) // 选择的字符
-  if (selectVariable) {
-    // 将选择的字符美化后打印
-    commands.executeCommand('editor.action.insertLineAfter').then(() => {
-      const lineNumber = selection.end.line + 2 // console 所在的行号
-      const logText = formatVar(selectVariable, lineNumber)
-      insertText(logText)
-    })
-  } else {
-    handleCursorInsert()
+  for (let index = 0; index < textEditor.selections.length; index++) {
+    const selection = textEditor.selections[index]
+    await handleCursorInsert({ selection, textEditor })
   }
 }
--- src/common/insertVariable/index.ts
+++ src/common/insertVariable/index.ts
@@ -1,89 +1,22 @@
 import {
-  window,
-  commands,
-  TextEditor,
   Range,
-  TextDocument,
-  TextLine,
   Position
 } from 'vscode'
 import { formatVar } from '..'
 
-const { activeTextEditor, onDidChangeActiveTextEditor } = window
-let currentEditor: TextEditor
-export const insertVariable = context => {
-  currentEditor = activeTextEditor
-  // 当触发文本时重新赋值
-  onDidChangeActiveTextEditor(editor => (currentEditor = editor))
-  // 订阅命令
-  context.subscriptions.push(
-    commands.registerTextEditorCommand('consoleLog.insertVariable', () =>
-      handleCursorInsert()
-    )
-  )
-}
-
-interface WrapData {
-  txt: string
-  item: string
-  sel: Selection
-  doc: TextDocument
-  ran: Range
-  ind: string
-  idx: number
-  line: number
-  isLastLine: boolean
-}
-
 // 处理回调事件
-export const handleCursorInsert = () => {
-  new Promise((resolve, reject) => {
-    const doc = currentEditor.document
-    // 选中的文本对象
-    const sel = currentEditor.selection
-    // 获取文本位置
-    const ran = doc.getWordRangeAtPosition(sel.anchor) || new Range(sel.start, sel.end)
-    const lineNumber = ran.start.line
-    // 通过范围获取文本
-    const item = doc.getText(ran)
-    // doc.lineAt(lineNumber) 光标所在(整)行内容对象
-    // 第一个非空白字符索引
-    const idx = doc.lineAt(lineNumber).firstNonWhitespaceCharacterIndex
-    // 截取非空字符长度
-    // doc.lineAt(lineNumber).text 光标所在(整)行内容文本
-    // 光标所在行，第一个非空字符前的空字符串
-    const ind = doc.lineAt(lineNumber).text.substring(0, idx)
-    // TODO: 后期可以在这里对选择项为空时特殊处理
-    const logText = formatVar(item, lineNumber + 2)
-    const wrapData: any = {
-      txt: logText,
-      item: item,
-      doc: doc,
-      ran: ran,
-      ind: ind,
-      line: lineNumber,
-      sel: sel,
-      isLastLine: doc.lineCount - 1 === lineNumber
-    }
-    resolve(wrapData)
-  }).then((wrap: WrapData) => {
-    let nxtLine: TextLine
-    let nxtLineIndex: string
-    // 如果不是最后一行
-    if (!wrap.isLastLine) {
-      nxtLine = wrap.doc.lineAt(wrap.line + 1) // 获取下一行文本对象
-      // 截取下一行文本的第一个非空字符前的所有字符
-      nxtLineIndex = nxtLine.text.substring(
-        0,
-        nxtLine.firstNonWhitespaceCharacterIndex
-      )
-    }
-    currentEditor.edit(e => {
-      e.insert(
-        new Position(wrap.line, wrap.doc.lineAt(wrap.line).range.end.character),
-        // 比较下一行与光标所在行，第一个非空字符前的空字符串长度，以长的为准
-        '\n'.concat(nxtLineIndex > wrap.ind ? nxtLineIndex : wrap.ind, wrap.txt)
-      )
-    })
+export const handleCursorInsert = async ({selection, textEditor}) => {
+  const document = textEditor.document
+  // 获取文本位置
+  const range = document.getWordRangeAtPosition(selection.anchor) || new Range(selection.start, selection.end)
+  const lineNumber = range.start.line
+  // 通过范围获取文本
+  const logText = formatVar(document.getText(range), lineNumber + 2)
+
+  await textEditor.edit(e => {
+    e.insert(
+      new Position(lineNumber, document.lineAt(lineNumber).range.end.character),
+      '\n'.concat(logText)
+    )
   })
 }
