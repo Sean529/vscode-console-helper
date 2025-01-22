a635be74c9e46db1fa8ec57e060ea69b72bafb05
AT <atxiesz@gmail.com>
2025-01-22 11:33:24 +0800

    fix: 点操作符变量打印不完整的问题

--- src/common/insertVariable/index.ts
+++ src/common/insertVariable/index.ts
@@ -5,7 +5,7 @@ import { formatVar } from ".."
 export const handleCursorInsert = async ({ selection, textEditor }) => {
   const document = textEditor.document
   // 获取文本位置
-  const range = document.getWordRangeAtPosition(selection.anchor) || new Range(selection.start, selection.end)
+  const range = document.getWordRangeAtPosition(selection.anchor, /[a-zA-Z_$][a-zA-Z0-9_$.]*/) || new Range(selection.start, selection.end)
   const lineNumber = range.start.line
   // 通过范围获取文本
   const logText = formatVar(document.getText(range), lineNumber + 2)
